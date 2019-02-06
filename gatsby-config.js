dbc8b95c28cc675eacabc4d7bf361727e0edd0c1
Aaron Lampros <alampros@gmail.com>
2019-02-06 13:58:19 -0500

    build: add webmanifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,5 +33,18 @@ module.exports = {
         trackingId: 'UA-133753267-1',
       },
     },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Aaron Lampros | Resume',
+        short_name: '@alampros',
+        start_url: '/',
+        background_color: '#4FC1B7',
+        theme_color: '#4FC1B7',
+        display: 'standalone',
+        icon: 'static/aaron-sm.jpg',
+        include_favicon: true,
+      },
+    },
   ],
 }
