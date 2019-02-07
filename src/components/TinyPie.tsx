202b573b08bc54459245a1df145332cbc1185569
Aaron Lampros <alampros@gmail.com>
2019-02-07 14:03:30 -0500

    fix tinypie in safari

--- src/components/TinyPie.tsx
+++ src/components/TinyPie.tsx
@@ -14,12 +14,16 @@ export default class TinyPie extends React.Component<Props & React.SVGAttributes
       ...passedProps
     } = this.props
     const perc = value * 100
-    const s = {
-      strokeDasharray: `${perc} ${100 - perc}`,
-    }
     return (
-      <svg viewBox="0 0 32 32" className={cx(styles.root, className)} {...passedProps}>
-        <circle r="25%" cx="50%" cy="50%" className={styles.part} style={s} pathLength={100} />
+      <svg viewBox="0 0 36 36" className={cx(styles.root, className)} {...passedProps}>
+        <path
+          className={styles.part}
+          strokeDasharray={`${perc / 2}, 100`}
+          d={`M18 10
+            a 9 9 0 0 1 0 18
+            a 9 9 0 0 1 0 -18`
+          }
+        />
       </svg>
     )
   }
