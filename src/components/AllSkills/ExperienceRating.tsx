95c3f4f01d38e2a016ca338f324aa2993eb1a084
Aaron Lampros <alampros@gmail.com>
2019-02-08 15:28:33 -0500

    feat: Fancy pluralizations with fractions
    close #6

--- src/components/AllSkills/ExperienceRating.tsx
+++ src/components/AllSkills/ExperienceRating.tsx
@@ -6,6 +6,16 @@ import { ISkill } from 'data/Skill'
 type Props = any & Pick<ISkill, 'yearsOfExperience'>
 
 function ExperienceRating({ yearsOfExperience, ...passedProps }: Props) {
+  const plural = yearsOfExperience > 1 ? 'years' : 'year'
+  const change = yearsOfExperience % 1
+  const bits = (() => {
+    if(!change) return ''
+    if(change === 0.5) return '½'
+    if(change === 0.25) return '¼'
+    if(change === 0.75) return '¾'
+    return change.toFixed(1).slice(1)
+  })()
+  const years = yearsOfExperience < 1 ? bits : Math.floor(yearsOfExperience) + bits
   return (
     <Pane
       display="flex"
@@ -13,7 +23,9 @@ function ExperienceRating({ yearsOfExperience, ...passedProps }: Props) {
       {...passedProps}
     >
       <IoIosTime />
-      <span>{yearsOfExperience}</span>
+      <small>
+        <strong>{years}</strong> {plural}
+      </small>
     </Pane>
   )
 }
