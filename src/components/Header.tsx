fcb5169a081c19fc9698d9356742f770e486c2a0
Aaron Lampros <alampros@gmail.com>
2019-02-12 17:05:02 -0500

    file banners

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,9 @@ import {
   Button,
   Pane,
 } from 'evergreen-ui'
+import Address from 'components/Address'
 import { IResumeMetadata } from 'data/ResumeMetadata'
-import { MdPermPhoneMsg, MdMyLocation, MdEmail, MdPictureAsPdf, MdPermContactCalendar } from 'react-icons/md'
+import { MdPermPhoneMsg, MdMyLocation, MdEmail } from 'react-icons/md'
 import { GoMarkGithub } from 'react-icons/go'
 const styles = require('./Header.module.css')
 
@@ -24,68 +25,69 @@ export default class Header extends React.Component<Props> {
       github,
     } = this.props
     const middle = middleName && middleName.length === 1 ? middleName + '.' : middleName
-    const nonEvergreenIconStyles = {
-      width: '1.25em',
-      height: '1.25em',
-      marginRight: '0.5em',
-    }
     return (
       <header className={styles.root}>
-        <span>
-          <h1 className={styles.name} aria-label={`${firstName} ${middle} ${lastName}`}>
-            <span aria-hidden>{firstName}</span><span aria-hidden>{middle}</span><span aria-hidden>{lastName}</span>
-          </h1>
-          <small>User Experience Architect</small>
-        </span>
         <Pane
-          className={styles.contact}
           display="flex"
           flexDirection="row"
+          justifyContent="space-between"
         >
+          <span>
+            <h1 className={styles.name} aria-label={`${firstName} ${middle} ${lastName}`}>
+              <span aria-hidden>{firstName}</span><span aria-hidden>{middle}</span><span aria-hidden>{lastName}</span>
+            </h1>
+            <small>User Experience Architect</small>
+          </span>
           <Pane
-            className={cx('no-print', styles.actions)}
+            className={styles.contact}
             display="flex"
-            flexDirection="column"
-            borderRight="default"
-            alignItems="flex-end"
-            justifyContent="center"
+            flexDirection="row"
           >
-            <Button
-              is="a"
-              href="/resume-alampros.pdf"
-              appearance="minimal"
-            >
-              <MdPictureAsPdf style={nonEvergreenIconStyles} />
-              resume.pdf
-            </Button>
-            <Button
-              is="a"
-              href="/Aaron Lampros.vcf"
-              appearance="minimal"
-            >
-              <MdPermContactCalendar style={nonEvergreenIconStyles} />
-              Add me to your contacts
-            </Button>
+            <address>
+              <div>
+                <MdMyLocation />
+                <Address address={location} />
+              </div>
+              <div>
+                <MdPermPhoneMsg />
+                <a href={`tel:${phone}`}>{phone}</a>
+              </div>
+              <div>
+                <GoMarkGithub />
+                <a href={`https://github.com/${github}`}>github.com/alampros</a>
+              </div>
+              <div>
+                <MdEmail />
+                <a href={`mailto:${email}`}>{email}</a>
+              </div>
+            </address>
           </Pane>
-          <address>
-            <div>
-              <MdMyLocation />
-              <span>{location}</span>
-            </div>
-            <div>
-              <MdPermPhoneMsg />
-              <a href={`tel:${phone}`}>{phone}</a>
-            </div>
-            <div>
-              <GoMarkGithub />
-              <a href={`https://github.com/${github}`}>github.com/alampros</a>
-            </div>
-            <div>
-              <MdEmail />
-              <a href={`mailto:${email}`}>{email}</a>
-            </div>
-          </address>
         </Pane>
+        <div
+          className={cx('no-print', styles.actions)}
+          role="menubar"
+        >
+          <Button
+            is="a"
+            href="/resume-alampros.pdf"
+            appearance="minimal"
+            iconBefore="download"
+          >
+            <span className={styles.shadow}>
+              Download PDF
+            </span>
+          </Button>
+          <Button
+            is="a"
+            href="/Aaron Lampros.vcf"
+            appearance="minimal"
+            iconBefore="user"
+          >
+            <span className={styles.shadow}>
+            Add me to your contacts
+            </span>
+          </Button>
+        </div>
       </header>
     )
   }
