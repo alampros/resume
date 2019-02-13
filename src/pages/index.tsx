14c170ace67ad8a6ac27150ecfade72b33230d22
Aaron Lampros <alampros@gmail.com>
2019-02-13 13:00:04 -0500

    Revert "feat: remove objective"
    This reverts commit e65412997c6ad71c7f5d70caaa51f9fa664d4a13.
    
    # Conflicts:
    #	src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import Layout from 'components/Layout'
 import allJobs from 'data/jobs'
 import Jobs from 'components/Jobs'
+import Objective from 'components/Objective'
 import AllSkills from 'components/AllSkills'
 import skillsById from 'data/skills'
 
@@ -10,6 +11,8 @@ export default class IndexPage extends React.Component {
   render() {
     return (
       <Layout>
+        <Objective />
+        <hr />
         <Jobs jobs={allJobs} />
         <hr />
         <AllSkills skills={skills} />
