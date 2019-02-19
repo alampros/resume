a870c4c4f2c10d0c1f4f98f3143d8a46edeba667
Aaron Lampros <alampros@gmail.com>
2019-02-19 15:59:29 -0500

    Remove skills search
    Fix #24

--- src/components/AllSkills/AllSkills.tsx
+++ src/components/AllSkills/AllSkills.tsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react'
 import {
   Pane,
-  SearchInput,
   SegmentedControl,
   Small,
 } from 'evergreen-ui'
-import Fuse from 'fuse.js'
 import { ISkill } from 'data/Skill'
 import SkillsList from './SkillsList'
 
@@ -33,26 +31,14 @@ interface Props {
 }
 
 function AllSkills(props: Props) {
-  const [filterStr, setFilterStr] = useState('')
   const [sortBy, setSortBy] = useState('strength')
   const {
     skills,
   } = props
-  const fuse = new Fuse(skills, {
-    shouldSort: true,
-    threshold: 0.6,
-    location: 0,
-    distance: 16,
-    maxPatternLength: 25,
-    minMatchCharLength: 1,
-    keys: [
-      'name',
-    ],
-  })
   if(!skills || !skills.length) {
     return null
   }
-  const filteredSkills = (filterStr === '' ? skills : fuse.search(filterStr))
+  const sortedSkills = skills
     .sort((a, b) => {
       const va = a[sortBy as ISortKey]
       const vb = b[sortBy as ISortKey]
@@ -75,15 +61,6 @@ function AllSkills(props: Props) {
           alignItems="flex-end"
           className="no-print"
         >
-          <SearchInput
-            aria-label="Filter Skills"
-            marginBottom="0.5rem"
-            value={filterStr}
-            width="100%"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-              return setFilterStr(e.currentTarget.value)
-            }}
-          />
           <Pane display="flex" flex-direction="row" align-items="center">
             <Small is="label" htmlFor="sortBy" marginRight="0.5rem">Sort By:</Small>
             <SegmentedControl
@@ -99,7 +76,7 @@ function AllSkills(props: Props) {
           </Pane>
         </Pane>
       </Pane>
-      <SkillsList skills={filteredSkills} />
+      <SkillsList skills={sortedSkills} />
     </section>
   )
 }
