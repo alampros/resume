6d204ff80d28e12b7179d568eba1be0878b06aab
Aaron Lampros <alampros@gmail.com>
2019-02-21 14:50:21 -0500

    Add link to github repo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import cx from 'classnames'
 import { MdPictureAsPdf } from 'react-icons/md'
+import { GoMarkGithub } from 'react-icons/go'
 import { QRCode } from 'react-qr-svg'
 const styles = require('./Footer.module.css')
 
@@ -8,6 +9,10 @@ export default class Footer extends React.Component {
   render() {
     return (
       <footer className={styles.root}>
+        <a href="https://github.com/alampros/resume" className={styles.ghLink}>
+          <GoMarkGithub />
+          <span>View the source for this site</span>
+        </a>
         <a href="/resume-alampros.pdf" className={cx('no-print', styles.resumeLink)}>
           <MdPictureAsPdf />
           Download my resume (<abbr title="Portable Document Format">PDF</abbr>)
